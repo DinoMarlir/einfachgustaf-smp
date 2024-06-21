c0ee345459598a0e2dec127cdead738957fa80c3
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-06-21 14:54:28 +0200

    removed client entrypoint

--- src/main/kotlin/live/einfachgustaf/mods/smp/Entrypoint.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/Entrypoint.kt
@@ -1,20 +1,15 @@
 package live.einfachgustaf.mods.smp
 
-import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.DedicatedServerModInitializer
 import net.fabricmc.api.ModInitializer
 import org.apache.logging.log4j.LogManager
 
-object Entrypoint: ModInitializer, DedicatedServerModInitializer, ClientModInitializer {
+object Entrypoint: ModInitializer, DedicatedServerModInitializer {
 
     val logger = LogManager.getLogger("smp")
     
     override fun onInitialize() {
         // Common initialization
-    }
-
-    override fun onInitializeClient() {
-        // Client initialization
     }
 
     override fun onInitializeServer() {
