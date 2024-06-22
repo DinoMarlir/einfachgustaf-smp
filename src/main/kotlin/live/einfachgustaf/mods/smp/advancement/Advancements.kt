b1133f2992d33ec373f6aba70310657c83008573
mathearbeit76 <173494459+mathearbeit76@users.noreply.github.com>
2024-06-22 13:25:08 +0200

    Advancement registration

--- src/main/kotlin/live/einfachgustaf/mods/smp/advancement/Advancements.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/advancement/Advancements.kt
@@ -1,17 +1,27 @@
 package live.einfachgustaf.mods.smp.advancement
 
-import net.minecraft.advancements.Advancement
-import net.minecraft.advancements.AdvancementHolder
-import net.minecraft.advancements.DisplayInfo
+import live.einfachgustaf.mods.smp.LOGGER
+import net.minecraft.advancements.*
+import net.minecraft.advancements.critereon.ImpossibleTrigger
+import net.minecraft.network.protocol.game.ClientboundUpdateAdvancementsPacket
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
 import java.util.*
 
 object Advancements {
-
     val DEFAULT_RESOURCE = ResourceLocation("textures/gui/advancements/backgrounds/adventure.png")
 
-    fun createAdvancementEntry(forAdvancement: GustafAdvancement): AdvancementHolder {
+    private val advancements = mutableListOf<AdvancementHolder>()
+
+    fun advancement(path: String): AdvancementHolder? {
+        return advancements.firstOrNull { it.id == res(path) }
+    }
+
+    private fun res(path: String): ResourceLocation {
+        return ResourceLocation("einfachgustaf:/root/$path")
+    }
+
+    fun register(forAdvancement: GustafAdvancement, path: String, parent: AdvancementHolder? = null): AdvancementHolder {
         val entry = Advancement.Builder.advancement()
             .display(DisplayInfo(
                 forAdvancement.displayIcon,
@@ -20,14 +30,44 @@ object Advancements {
                 Optional.of(forAdvancement.backgroundResource),
                 forAdvancement.type,
                 true,
-                false,
-                false
+                true,
+                true
             ))
-            .build(ResourceLocation("einfachgustaf:/root"))
-        return entry
+            .addCriterion("dummy", CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance()))
+        if (parent != null) {
+            entry.parent(parent)
+        }
+        val advancementHolder = entry.build(res(path))
+        advancements += advancementHolder
+        return advancementHolder
     }
 
-    fun ServerPlayer.awardAdvancement(advancement: AdvancementHolder) {
-        advancements.award(advancement, "dummy")
+    fun createAdvancements(serverPlayer: ServerPlayer) {
+        LOGGER.info("create")
+        serverPlayer.connection.send(
+            ClientboundUpdateAdvancementsPacket(
+                false,
+                advancements,
+                setOf(),
+                //advancements.associate { it.id to AdvancementProgress() }
+                mapOf()
+            )
+        )
+
+    }
+
+    fun awardAdvancement(serverPlayer: ServerPlayer, advancement: AdvancementHolder) {
+        val progress = AdvancementProgress()
+        val requirements = AdvancementRequirements.allOf(listOf("dummy"))
+        progress.update(requirements)
+        progress.grantProgress("dummy")
+        serverPlayer.connection.send(
+            ClientboundUpdateAdvancementsPacket(
+                false,
+                listOf(),
+                setOf(),
+                mapOf(advancement.id to progress)
+            )
+        )
     }
 }
