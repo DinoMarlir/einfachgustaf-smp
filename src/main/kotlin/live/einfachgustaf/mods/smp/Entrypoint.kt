bdfaf37104d9b0487167b528c6f64c28abcd6fe6
mathearbeit76 <173494459+mathearbeit76@users.noreply.github.com>
2024-06-22 12:48:25 +0200

    Implement /notify command

--- src/main/kotlin/live/einfachgustaf/mods/smp/Entrypoint.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/Entrypoint.kt
@@ -1,15 +1,30 @@
 package live.einfachgustaf.mods.smp
 
-import net.minecraft.world.item.Items
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.commands.Commands
+import net.minecraft.commands.arguments.item.ItemArgument
+import net.minecraft.commands.arguments.item.ItemInput
+import net.minecraft.data.registries.VanillaRegistries
 import net.silkmc.silk.commands.command
 import net.silkmc.silk.core.text.literalText
 import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
-val LOGGER = LogManager.getLogger("smp")
+val LOGGER: Logger = LogManager.getLogger("smp")
     
 fun initMain() {
 }
 
 fun initServer() {
+    val context = Commands.createValidationContext(VanillaRegistries.createLookup())
+    command("notify") {
+        argument<ItemInput>("item", ItemArgument.item(context)) { item ->
+            argument<String>("text") { text ->
+                runs {
+                    source.playerOrException.sendNotifcation(literalText(text.invoke(this)), item.invoke(this).createItemStack(1, false))
+                }
+            }
+        }
+    }
 
 }
--- src/main/kotlin/live/einfachgustaf/mods/smp/ServerPlayerExtensions.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/ServerPlayerExtensions.kt
@@ -3,43 +3,38 @@ package live.einfachgustaf.mods.smp
 import live.einfachgustaf.mods.smp.advancement.Advancements
 import net.minecraft.advancements.*
 import net.minecraft.advancements.critereon.PlayerTrigger
-import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.network.chat.Component
 import net.minecraft.network.protocol.game.ClientboundUpdateAdvancementsPacket
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.item.ItemStack
-import java.time.Instant
+import net.silkmc.silk.core.text.literalText
 import java.util.*
 
 /**
  * Sends a notification toast to the player
- * @param title - Title of the notifcation. Won't be shown in  the toast.
- * @param description - Content of the notification
+ * @param content - Content of the notification
  * @param icon - Item to be shown as icon
  */
-fun ServerPlayer.sendNotifcation(title: Component, description: Component, icon: ItemStack) {
-   val advancement=
-        Advancement.Builder.advancement()
-            .display(DisplayInfo(
-                icon,
-                title,
-                description,
-                Optional.of(Advancements.DEFAULT_RESOURCE),
-                AdvancementType.TASK,
-                true,
-                true,
-                true
-            ))
-            .addCriterion("dummy", PlayerTrigger.TriggerInstance.tick())
-            .build(ResourceLocation("einfachgustaf:/root/dummy"))
+fun ServerPlayer.sendNotifcation(content: Component, icon: ItemStack) {
+   val advancement = Advancement.Builder.advancement()
+        .display(DisplayInfo(
+            icon,
+            content,
+            literalText("This should not appear ingame"),
+            Optional.of(Advancements.DEFAULT_RESOURCE),
+            AdvancementType.TASK,
+            true,
+            true,
+            true
+        ))
+        .addCriterion("dummy", PlayerTrigger.TriggerInstance.tick())
+        .build(ResourceLocation("einfachgustaf:/root/dummy"))
 
     val progress = AdvancementProgress()
-
     val requirements = AdvancementRequirements.allOf(listOf("dummy"))
     progress.update(requirements)
     progress.grantProgress("dummy")
-
     connection.send(
         ClientboundUpdateAdvancementsPacket(
             false,
@@ -48,5 +43,5 @@ fun ServerPlayer.sendNotifcation(title: Component, description: Component, icon:
             mapOf(advancement.id to progress)
         )
     )
-    LOGGER.info("Sent packet")
+    LOGGER.debug("Sent advancement packet to $stringUUID")
 }
