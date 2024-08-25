c6d67ad4a8f33ceb7b93ba24cba322f9f2af1939
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-08-25 12:41:32 +0200

    Revert "fixed errors"
    This reverts commit d610314f6721e5687b17e9c471a1c3b09e0b2df0.

--- src/main/kotlin/live/einfachgustaf/mods/smp/advancement/Advancements.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/advancement/Advancements.kt
@@ -11,7 +11,6 @@ import net.minecraft.network.protocol.game.ClientboundUpdateAdvancementsPacket
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.item.ItemEntity
-import net.minecraft.world.entity.player.Player
 import net.silkmc.silk.core.task.mcCoroutineScope
 
 object Advancements {
@@ -115,7 +114,7 @@ object Advancements {
         )
     }
 
-    suspend fun awardAdvancement(serverPlayer: Player, advancement: CompilableAdvancement, isRestore: Boolean = false, ignoreDuplicate: Boolean = false) {
+    suspend fun awardAdvancement(serverPlayer: ServerPlayer, advancement: CompilableAdvancement, isRestore: Boolean = false, ignoreDuplicate: Boolean = false) {
         var restoreOrDuplicate = isRestore
         val progress = AdvancementProgress()
         val requirements = AdvancementRequirements.allOf(listOf("dummy"))
--- src/main/kotlin/live/einfachgustaf/mods/smp/advancement/impl/normal/ATrueGustaf.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/advancement/impl/normal/ATrueGustaf.kt
@@ -1,44 +1,48 @@
-package live.einfachgustaf.mods.smp.advancement.impl.normal
+package live.einfachgustaf.mods.smp.advancement.impl.beginner
 
 import kotlinx.coroutines.launch
 import live.einfachgustaf.mods.smp.advancement.Advancements
 import live.einfachgustaf.mods.smp.advancement.GustafAdvancement
 import live.einfachgustaf.mods.smp.advancement.impl.beginnerRoot
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback
+import net.fabricmc.fabric.api.event.player.PlayerCraftsItemCallback
 import net.minecraft.advancements.AdvancementType
-import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.Blocks
+import net.silkmc.silk.core.item.itemStack
 import net.silkmc.silk.core.task.mcCoroutineScope
 import net.silkmc.silk.core.text.literalText
 
-object JukeBoxen {
+object ATrueGustaf {
 
     fun register() {
         val advancement = Advancements.register(
             GustafAdvancement(
                 Blocks.JUKEBOX.asItem().defaultInstance,
-                literalText("Juke-Boxen"),
-                literalText("Schlage einen anderen Spieler mit einer Jukebox."),
+                literalText("Ein wahrer Gustaf"),
+                literalText("Crafte ein Jukebox."),
                 AdvancementType.TASK,
                 isUnlocked = true,
                 rewards = setOf(
-                    Items.JUKEBOX.defaultInstance,
+                    Items.DIAMOND.defaultInstance,
                 )
             ),
-            "jukeboxen",
+            "atruegustaf",
             beginnerRoot,
             x = 1.5f
         )
 
-        AttackEntityCallback.EVENT.register { player, world, hand, entity, hitResult ->
-            if (entity is Player && player.getItemInHand(hand).item == Blocks.JUKEBOX.asItem()) {
+        // Ereignis-Listener für das Crafting-Event
+        PlayerCraftsItemCallback.EVENT.register { player, craftedStack, craftingInventory ->
+            // Prüfe, ob das gecraftete Item eine Jukebox ist
+            if (craftedStack.item == Blocks.JUKEBOX.asItem()) {
 
+                // Vergib das Advancement
                 mcCoroutineScope.launch {
                     Advancements.awardAdvancement(player, advancement)
                 }
             }
 
+            // Rückgabewert für das Event (standardmäßig KEIN Problem, um andere Event-Listener nicht zu stören)
             ActionResult.PASS
         }
     }
