6d6f2b8caaa9230034b83b28f1e79ae185efb49e
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-08-25 16:40:32 +0200

    optimize WhatTimeIsIt advancement

--- src/main/kotlin/live/einfachgustaf/mods/smp/advancement/impl/beginner/WhatTimeIsIt.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/advancement/impl/beginner/WhatTimeIsIt.kt
@@ -1,17 +1,28 @@
 package live.einfachgustaf.mods.smp.advancement.impl.beginner
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Job
 import live.einfachgustaf.mods.smp.advancement.Advancements
+import live.einfachgustaf.mods.smp.advancement.CompilableAdvancement
 import live.einfachgustaf.mods.smp.advancement.GustafAdvancement
 import live.einfachgustaf.mods.smp.advancement.impl.beginnerRoot
+import live.einfachgustaf.mods.smp.event.PlayerCollectItemEvent
+import live.einfachgustaf.mods.smp.event.PlayerCraftItemEvent
+import live.einfachgustaf.mods.smp.event.PlayerDropItemEvent
+import live.einfachgustaf.mods.smp.extensions.asServerPlayer
+import me.obsilabor.alert.kotlin.subscribeToEvent
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 import net.minecraft.advancements.AdvancementType
+import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.item.Items
-import net.silkmc.silk.core.task.mcCoroutineScope
+import net.silkmc.silk.core.task.mcCoroutineTask
 import net.silkmc.silk.core.text.literalText
+import kotlin.time.Duration.Companion.minutes
+
+// TODO: don't run job if player has the advancement already
 
 object WhatTimeIsIt {
+
+    val playerJobMap = mutableMapOf<ServerPlayer, Job>()
 
     fun register() {
         val advancement = Advancements.register(
@@ -30,33 +41,51 @@ object WhatTimeIsIt {
             x = 1.5f
         )
 
+        collectionEvents(advancement)
+    }
+
+    private fun collectionEvents(advancement: CompilableAdvancement) {
         ServerPlayConnectionEvents.JOIN.register { event, _, _ ->
             val player = event.player
-            mcCoroutineScope.launch {
-                while (!player.inventory.contains(Items.CLOCK.defaultInstance)) {
-                    delay(1000)
-                }
 
-                val startTime = System.currentTimeMillis()
-                var hasClockFor20Minutes = false
-
-                while (!hasClockFor20Minutes) {
-                    if (!player.inventory.contains(Items.CLOCK.defaultInstance)) {
-                        break
-                    }
-
-                    val elapsedTime = System.currentTimeMillis() - startTime
-                    if (elapsedTime >= 20 * 60 * 1000) {
-                        hasClockFor20Minutes = true
-                    }
-
-                    delay(1000)
-                }
-
-                if (hasClockFor20Minutes) {
-                    Advancements.awardAdvancement(player, advancement)
-                }
+            if (player.inventory.items.any { it.item == Items.CLOCK }) {
+                startDelayedJob(player, advancement)
             }
         }
+
+        subscribeToEvent<PlayerCraftItemEvent> {
+            if (it.item.item == Items.CLOCK) {
+                startDelayedJob(it.player.asServerPlayer() ?: return@subscribeToEvent, advancement)
+            }
+        }
+
+        subscribeToEvent<PlayerCollectItemEvent> {
+            if (it.item.item == Items.CLOCK) {
+                startDelayedJob(it.player.asServerPlayer() ?: return@subscribeToEvent, advancement)
+            }
+        }
+    }
+
+    private fun removeEvents() {
+        subscribeToEvent<PlayerDropItemEvent> {
+            cancelJobForPlayer(it.player)
+        }
+    }
+
+    private fun startDelayedJob(player: ServerPlayer, advancement: CompilableAdvancement): Job {
+        val job = mcCoroutineTask(delay = 20.minutes) {
+            if (player.inventory.items.any { it.item == Items.CLOCK }) {
+                Advancements.awardAdvancement(player, advancement)
+            }
+        }
+
+        playerJobMap[player] = job
+        return job
+    }
+
+    fun cancelJobForPlayer(player: ServerPlayer) {
+        val job = playerJobMap[player] ?: return
+        job.cancel()
+        playerJobMap.remove(player)
     }
 }
