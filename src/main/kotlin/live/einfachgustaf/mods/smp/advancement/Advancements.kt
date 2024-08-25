35dc2dd6bad0e58dd9cab1c87d7cc266fd5cdd7d
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-08-25 13:17:12 +0200

    implement multiple advancement tabs

--- src/main/kotlin/live/einfachgustaf/mods/smp/advancement/Advancements.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/advancement/Advancements.kt
@@ -55,8 +55,8 @@ object Advancements {
      * @param gustafAdvancement the root advancement for the tab
      * @return the registered root-advancement in a pre-compiled state
      */
-    fun createTab(gustafAdvancement: GustafAdvancement): CompilableAdvancement {
-        val compiledAdvancement = CompilableAdvancement(gustafAdvancement, path = "")
+    fun createTab(path: String = "", gustafAdvancement: GustafAdvancement): CompilableAdvancement {
+        val compiledAdvancement = CompilableAdvancement(gustafAdvancement, path = path)
         advancements += compiledAdvancement
         root = compiledAdvancement
         return compiledAdvancement
