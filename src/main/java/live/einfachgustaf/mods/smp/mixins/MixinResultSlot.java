d66a4d61c21cb46719480e2d1466296d333ae9e9
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-08-30 09:59:00 +0200

    made wasQuickCrafted unique

--- src/main/java/live/einfachgustaf/mods/smp/mixins/MixinResultSlot.java
+++ src/main/java/live/einfachgustaf/mods/smp/mixins/MixinResultSlot.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,6 +20,7 @@ public class MixinResultSlot {
     @Final
     private Player player;
 
+    @Unique
     private boolean wasQuickCrafted = false;
 
     @Inject(at = @At("HEAD"), method = "onTake")
