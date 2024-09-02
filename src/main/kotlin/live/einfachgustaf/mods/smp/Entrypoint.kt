14552a33ae850076fe4755249729a6c9f931047b
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-09-02 16:41:19 +0200

    optimized imports

--- src/main/kotlin/live/einfachgustaf/mods/smp/Entrypoint.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/Entrypoint.kt
@@ -1,27 +1,13 @@
 package live.einfachgustaf.mods.smp
 
-import com.mojang.brigadier.arguments.StringArgumentType
-import kotlinx.coroutines.launch
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import live.einfachgustaf.mods.smp.advancement.AdvancementRegistry
 import live.einfachgustaf.mods.smp.advancement.Advancements
-import live.einfachgustaf.mods.smp.data.ItemStackHolder
 import live.einfachgustaf.mods.smp.data.db.MongoDB
 import live.einfachgustaf.mods.smp.discord.DiscordBot
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
-import net.minecraft.advancements.AdvancementType
-import net.minecraft.commands.Commands
-import net.minecraft.commands.arguments.item.ItemArgument
-import net.minecraft.commands.arguments.item.ItemInput
-import net.minecraft.data.registries.VanillaRegistries
-import net.minecraft.resources.ResourceLocation
-import net.silkmc.silk.commands.command
 import net.silkmc.silk.core.annotations.ExperimentalSilkApi
 import net.silkmc.silk.core.event.Events
 import net.silkmc.silk.core.event.Server
-import net.silkmc.silk.core.task.mcCoroutineScope
-import net.silkmc.silk.core.text.literalText
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
