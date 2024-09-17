2c08ab7bd6d8ebc961fb8850bfe6c40ac7317e96
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-09-17 11:59:06 +0200

    added javadocs

--- src/main/kotlin/live/einfachgustaf/mods/smp/extensions/PlayerExtensions.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/extensions/PlayerExtensions.kt
@@ -4,6 +4,11 @@ import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.player.Player
 import net.silkmc.silk.core.Silk
 
+/**
+ * Extension function to convert a Player to a ServerPlayer.
+ *
+ * @return the corresponding ServerPlayer if it exists, null otherwise.
+ */
 fun Player.asServerPlayer(): ServerPlayer? {
     return Silk.server?.playerList?.getPlayer(this.uuid)
 }
