db4e8fed8747ebde2f32bdebd0d334dd7e2452fa
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-09-17 12:01:48 +0200

    added javadocs for all events

--- src/main/kotlin/live/einfachgustaf/mods/smp/event/CandleLightUpEvent.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/event/CandleLightUpEvent.kt
@@ -6,6 +6,14 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.state.BlockState
 
+/**
+ * Event triggered when a candle is lit up.
+ *
+ * @property player The player who lit the candle.
+ * @property itemStack The item stack used to light the candle.
+ * @property blockState The state of the block where the candle is located.
+ * @property blockPos The position of the block where the candle is located.
+ */
 class CandleLightUpEvent(
     val player: Player,
     val itemStack: ItemStack,
