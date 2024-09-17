5cc34be01aa25b1f84c987106ed51b7099a8010b
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-09-17 12:00:30 +0200

    added javadocs for events

--- src/main/kotlin/live/einfachgustaf/mods/smp/event/PlayerCollectItemEvent.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/event/PlayerCollectItemEvent.kt
@@ -4,6 +4,12 @@ import me.obsilabor.alert.Event
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 
+/**
+ * Event triggered when a player collects an item.
+ *
+ * @property player The player who collected the item.
+ * @property item The item stack that was collected.
+ */
 class PlayerCollectItemEvent(
     val player: Player,
     val item: ItemStack
