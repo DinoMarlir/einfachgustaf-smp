3a2b4e14668fef0b95f5bf8d5ad9af1207118352
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-10-05 12:23:18 +0200

    add javadocs for AbstractSMPTeam.kt

--- src/main/kotlin/live/einfachgustaf/mods/smp/teams/AbstractSMPTeam.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/teams/AbstractSMPTeam.kt
@@ -2,16 +2,55 @@ package live.einfachgustaf.mods.smp.teams
 
 import java.util.UUID
 
+/**
+ * Represents a team in the SMP.
+ */
 abstract class AbstractSMPTeam {
+
+    /**
+     * The UUID of the team.
+     *
+     * @return The UUID of the team.
+     */
     abstract fun uuid(): UUID
 
+    /**
+     * The name of the team.
+     *
+     * @return The name of the team.
+     */
     abstract fun name(): String
+
+    /**
+     * Sets the name of the team.
+     *
+     * @param name The name of the team.
+     */
     abstract fun name(name: String)
 
+    /**
+     * The color of the team.
+     *
+     * @return The color of the team.
+     */
     abstract fun color(): String
+
+    /**
+     * Sets the color of the team.
+     *
+     * @param color The color of the team.
+     */
     abstract fun color(color: String)
 
+    /**
+     * Updates the team.
+     */
     abstract fun update()
 
+    /**
+     * Serializes the team.
+     *
+     * @return The serialized team.
+     */
     fun serializable() = SerializableSMPTeam(uuid().toString(), name(), color())
 }
